75a9a7ee6ab976b516c12cd331f8252367dda918
Nikita Kuznetsov <nikita_kuznetsov@list.ru>
2021-12-02 17:24:17 +0300

    refactor

---
+++ Day1.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "Utils.h"
+
+struct Day1 {
+    std::vector<int> measurement = read_measurements_from_file("measurements.txt");
+
+    int problem1() {
+        int res = 0;
+        for (size_t i = 1; i < measurement.size(); ++i) {
+            if (measurement[i] > measurement[i - 1]) {
+                ++res;
+            }
+        }
+        return res;
+    }
+
+    int problem2() {
+        int res = 0;
+        int first = measurement[0] + measurement[1] + measurement[2];
+        int second = measurement[1] + measurement[2] + measurement[3];
+        for (size_t i = 1; i < measurement.size() - 1; ++i) {
+            if (second > first) {
+                ++res;
+            }
+            first = second;
+            second = measurement[i - 1] + measurement[i] + measurement[i + 1];
+        }
+        return res;
+    }
+};
--- Utils.h
+++ Utils.h
@@ -4,4 +4,14 @@
 #include <string>
 #include <fstream>
 
-std::vector<int> read_from_file(const std::string& path);
+std::vector<int> read_measurements_from_file(const std::string& path) {
+    std::ifstream input(path);
+    std::vector<int> res;
+    if (input.is_open()) {
+        int val;
+        while (input >> val) {
+            res.push_back(val);
+        }
+    }
+    return res;
+}
